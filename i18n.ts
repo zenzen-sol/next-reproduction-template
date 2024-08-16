565dc86e80e54bcb15de8f6b981f520f84153b25
Sol Irvine <sol@zenzen.io>
2024-08-17 08:33:47 +0900

    Update i18n.ts

--- i18n.ts
+++ i18n.ts
@@ -4,7 +4,7 @@ import { notFound } from "next/navigation";
 
 export default getRequestConfig(async ({ locale }) => {
 	// Validate that the incoming `locale` parameter is valid
-	if (!locales.includes(locale as any)) notFound();
+	if (!locales.includes(locale as "en" | "ja")) notFound();
 
 	return {
 		messages: (await import(`./messages/${locale}.json`)).default,
