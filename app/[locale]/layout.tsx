3a915201aa220927f7fe52f9a8cd9fbaf026e0ba
Sol Irvine <sol@zenzen.io>
2024-08-22 17:38:04 +0900

    Move fonts

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -7,32 +7,8 @@ import {
 	getTranslations,
 	unstable_setRequestLocale,
 } from "next-intl/server";
-import { Montserrat } from "next/font/google";
-import localFont from "next/font/local";
 import type { ReactNode } from "react";
-
-export const montserrat = Montserrat({
-	weight: ["300", "400", "600", "900"],
-	style: ["normal", "italic"],
-	subsets: ["latin"],
-	variable: "--font-montserrat",
-});
-
-export const freightBigPro = localFont({
-	src: [
-		{
-			path: "../fonts/freight-big-pro-light.otf",
-			weight: "300",
-			style: "normal",
-		},
-		{
-			path: "../fonts/freight-big-pro-light-italic.otf",
-			weight: "300",
-			style: "italic",
-		},
-	],
-	variable: "--font-freight-big-pro",
-});
+import { freightBigPro, montserrat } from "./font";
 
 type Props = {
 	children: ReactNode;
