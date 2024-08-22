51215b17137306be1eabc3274f18cfa1052de0fd
Sol Irvine <sol@zenzen.io>
2024-08-22 18:06:35 +0900

    Update page.tsx

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,10 +1,16 @@
-import { useTranslations } from "next-intl";
+"use server";
 
-export default function HomePage() {
+import { useTranslations } from "next-intl";
+import { unstable_setRequestLocale } from "next-intl/server";
+
+const HomePage = ({ params: { locale } }: { params: { locale: string } }) => {
+	unstable_setRequestLocale(locale);
 	const t = useTranslations("HomePage");
 	return (
 		<div className="mx-auto max-w-prose bg-blue-200 p-12 rounded-lg">
 			<h1>{t("title")}</h1>
 		</div>
 	);
-}
+};
+
+export default HomePage;
